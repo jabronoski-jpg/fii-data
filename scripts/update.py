b35ef41a0f1e05dcdca59d41933f2b30af3b5ae5
jabronoski-jpg <jabronoski@gmail.com>
2026-07-15 19:29:21 -0300

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -1,1 +1,12 @@
+from datetime import datetime
+import json
 
+dados = {
+    "ultima_atualizacao": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    "status": "OK"
+}
+
+with open("public/version.json", "w", encoding="utf-8") as arquivo:
+    json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+
+print("Arquivo version.json atualizado!")

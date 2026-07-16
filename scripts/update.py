dd8c56379aaaa32b3db0e774de43f3c21e1efa1b
jabronoski-jpg <jabronoski@gmail.com>
2026-07-15 21:10:43 -0300

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -1,18 +1,52 @@
 import requests
+import base64
+import pandas as pd
+from io import StringIO
+
 
 URL_B3 = "https://sistemaswebb3-listados.b3.com.br/fundsListedProxy/Search/GetDownload/eyJsYW5ndWFnZSI6InB0LWJyIiwidHlwZUZ1bmQiOiJGSUkifQ=="
 
 
-r = requests.get(URL_B3)
-
-print("Status:", r.status_code)
-print("Content-Type:", r.headers.get("Content-Type"))
-print("Content-Disposition:", r.headers.get("Content-Disposition"))
-print("Tamanho:", len(r.content))
-
-print("Primeiros bytes:")
-print(r.content[:100])
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "*/*",
+    "Referer": "https://www.b3.com.br/"
+}
 
 
-with open("arquivo_b3", "wb") as f:
-    f.write(r.content)
+r = requests.get(URL_B3, headers=headers)
+
+r.raise_for_status()
+
+
+# resposta da B3 em Base64
+texto_base64 = r.text
+
+
+# decodifica
+csv_bytes = base64.b64decode(texto_base64)
+
+
+# transforma em texto
+csv_texto = csv_bytes.decode(
+    "utf-8",
+    errors="ignore"
+)
+
+
+print(csv_texto[:500])
+
+
+# lê CSV
+df = pd.read_csv(
+    StringIO(csv_texto),
+    sep=";"
+)
+
+
+print("\nCOLUNAS:")
+print(df.columns.tolist())
+
+
+print("\nPRIMEIROS REGISTROS:")
+print(df.head())
